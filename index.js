27a4357ea2ea420b63a7818873e9ec76d92b1258
Leila Jorg Barbosa <leilajorg.barbosa@leilajorg.barbosa-C57CTF6Y06>
2022-10-06 08:14:33 +0800

    added vcard qr code

---
+++ index.js
@@ -0,0 +1,4 @@
+const qrcode = require('qrcode-terminal');
+
+console.log(qrcode.generate('WIFI:S:SOCIALDESK - 9th Floor;T:WPA;P:SocialD3$K09F;;'));
+console.log(qrcode.generate("BEGIN:VCARD\r\nFN:Leila Barbosa\r\nTEL;TYPE=cell:09978955893\r\nEMAIL;PREF;INTERNET:leila.barbosa@siteminder.com\r\nEND:VCARD"));

6311257a47f833123b646d3a0c436c650124d7d9
Leila Jorg Barbosa <leilajorg.barbosa@leilajorg.barbosa-C57CTF6Y06>
2022-10-11 09:25:01 +0800

    added prettier and formatted

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const qrcode = require('qrcode-terminal');
 
-console.log(qrcode.generate('WIFI:S:SOCIALDESK - 9th Floor;T:WPA;P:SocialD3$K09F;;'));
-console.log(qrcode.generate("BEGIN:VCARD\r\nFN:Leila Barbosa\r\nTEL;TYPE=cell:555555555\r\nEMAIL;PREF;INTERNET:leila.barbosa@siteminder.com\r\nEND:VCARD"));
+console.log(
+  qrcode.generate('WIFI:S:SOCIALDESK - 9th Floor;T:WPA;P:SocialD3$K09F;;')
+);
+console.log(
+  qrcode.generate(
+    'BEGIN:VCARD\r\nFN:Leila Barbosa\r\nTEL;TYPE=cell:555555555\r\nEMAIL;PREF;INTERNET:leila.barbosa@siteminder.com\r\nEND:VCARD'
+  )
+);
